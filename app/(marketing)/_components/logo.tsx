1fa784eb232271ba582d1a6b62825eb1b2974285
Dev-Adnani <dev.adnani26@gmail.com>
2025-03-09 17:51:51 +0530

    final landing page

--- app/(marketing)/_components/logo.tsx
+++ app/(marketing)/_components/logo.tsx
@@ -10,7 +10,21 @@ const font = Poppins({
 const Logo = () => {
   return (
     <div className="hidden md:flex items-center gap-x-2">
-      <Image src={"/logo.svg"} height={40} width={40} alt="logo" />
+      <Image
+        className="dark:hidden"
+        src={"/logo.svg"}
+        height={40}
+        width={40}
+        alt="logo"
+      />
+
+      <Image
+        className="hidden dark:block"
+        src={"/logo-dark.svg"}
+        height={40}
+        width={40}
+        alt="logo"
+      />
       <p className={cn("font-semibold", font.className)}>Structo</p>
     </div>
   );
